3b2c5e260db0fb2642a8aa0d8a4cbca5c4fd104c
paaaarv <pkumar@trinity.edu>
2022-01-24 17:03:54 -0600

    form added to app

---
+++ src/components/UserForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+export default class UserForm extends React.Component{
+
+    render(){
+        return(
+            <Form>
+                
+            </Form>
+        )
+    }
+}
